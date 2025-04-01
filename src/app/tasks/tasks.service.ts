f3947d0c5ffe925d7d1e40bc3b28c6060135e106
braedengoodwin <braedenagoodwin@hotmail.com>
2025-04-01 18:09:38 -0500

    first

---
+++ src/app/tasks/tasks.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  // root means we can inject this service anywhere in our application
+  providedIn: 'root',
+})
+export class TasksSerivce {}
