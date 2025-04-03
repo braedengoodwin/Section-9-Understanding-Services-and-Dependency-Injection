b181d1933f48169d150398c411bfcc79b2700354
braedengoodwin <braedenagoodwin@hotmail.com>
2025-04-02 19:15:45 -0500

    finished with the section, confusion

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,7 +1,33 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
-@Injectable({
-  // root means we can inject this service anywhere in our application
-  providedIn: 'root',
-})
-export class TasksSerivce {}
+// @Injectable({
+//   // root means we can inject this service anywhere in our application
+//   providedIn: 'root',
+// })
+export class TasksSerivce {
+  private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
+
+  allTasks = this.tasks.asReadonly();
+
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
+  }
+
+  updateTaskService(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+    this.loggingService.log('CHANGE TASK STATUS TO ' + newStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
+import { TasksSerivce } from './app/tasks/tasks.service';
+import { InjectionToken } from '@angular/core';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+//bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+export const TasksServiceToken = new InjectionToken<TasksSerivce>(
+  'tasks-service-token'
+);
+
+bootstrapApplication(AppComponent, {
+  providers: [{ provide: TasksServiceToken, useClass: TasksSerivce }],
+}).catch((err) => console.error(err));
